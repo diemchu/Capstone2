caae4d4c72e1f39ba07c6406d075087ddba3f442
ducdiem <chuducdiem1@gmail.com>
2024-09-02 23:22:53 +0900

    init

---
+++ src/test/java/com/capstone2/nanum/NanumWedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone2.nanum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanumWedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
