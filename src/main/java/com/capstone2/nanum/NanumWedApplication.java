186414b83ee311db8d8d243c4e18255478ad6e56
ducdiem <chuducdiem1@gmail.com>
2024-09-06 22:56:50 +0900

    저장

--- src/main/java/com/capstone2/nanum/NanumWedApplication.java
+++ src/main/java/com/capstone2/nanum/NanumWedApplication.java
@@ -9,11 +9,6 @@ public class NanumWedApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(NanumWedApplication.class, args);
-<<<<<<< HEAD
-=======
-		System.out.println("hello java");
-		System.out.println("hello java2222");
->>>>>>> c8a74d49ee7ca823c92f04ad3ca6481c6b9314a3
 	}
 
 }
