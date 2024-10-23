f7f300e287a2204d3ad279c4d0a9ab0d7e235d7d
ducdiem <chuducdiem1@gmail.com>
2024-10-24 02:02:14 +0900

    Add

---
+++ src/main/java/com/capstone2/nanum/database/JoinRoom.java
@@ -0,0 +1,16 @@
+package com.capstone2.nanum.database;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "joinRooms")
+@Data
+public class JoinRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private  Long userId;
+    private Long roomId;
+}
---
+++ src/main/java/com/capstone2/nanum/repo/JoinRoomRepository.java
@@ -0,0 +1,8 @@
+package com.capstone2.nanum.repo;
+
+import com.capstone2.nanum.database.JoinRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  JoinRoomRepository  extends JpaRepository< JoinRoom,Long> {
+
+}
