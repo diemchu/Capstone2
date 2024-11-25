cf156128f58ca18b6d67280ceb4108657152b510
ducdiem <chuducdiem1@gmail.com>
2024-11-26 00:44:17 +0900

    add comment list popup

--- src/main/java/com/capstone2/nanum/controller/CommentController.java
+++ src/main/java/com/capstone2/nanum/controller/CommentController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
@@ -13,14 +17,30 @@ public class CommentController {
     @Autowired
     private com.capstone2.nanum.repo.CommentRepository commentRepository;
 
-    @PostMapping
+    @PostMapping("/add_comment")
     public ResponseEntity<?> addComment(@RequestBody Comment newComment) {
         try {
             commentRepository.save(newComment);
-            return ResponseEntity.ok().body("Comment added successfully");
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", true);
+            response.put("message", "Comment added successfully");
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error occurred while adding comment");
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", false);
+            response.put("message", "Error occurred while adding comment");
+            return ResponseEntity.status(500).body(response);
         }
     }
+    @GetMapping("/get_comments/{posterId}")
+    public ResponseEntity<List<Comment>> getComments(@PathVariable Long posterId) {
+        try {
+            List<Comment> comments = commentRepository.findByPosterId(posterId);
+            return ResponseEntity.ok(comments);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+
 }
 
--- src/main/java/com/capstone2/nanum/repo/CommentRepository.java
+++ src/main/java/com/capstone2/nanum/repo/CommentRepository.java
@@ -3,6 +3,9 @@ package com.capstone2.nanum.repo;
 import com.capstone2.nanum.database.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPosterId(Long posterId);
 }
 
